ebce32267509b66cd1ad7fe1610ef26a7242de4c
SamuelGuedesCanario <samuelgcanario1618@gmail.com>
2025-01-17 08:29:45 -0300

    Arquivos wokwi

---
+++ Keypad4x4/keypad2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/clocks.h"
+#include "hardware/pwm.h"
+
+// Biblioteca padrão do Raspberry Pi Pico para manipulação de GPIOs e comunicação serial
